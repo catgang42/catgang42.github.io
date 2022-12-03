efeb8f53218800f3d0ea3f2c3be08c9a9cad7604
xpchips <imdefosus@gmail.com>
2022-12-03 22:08:23 +0000

    Minecaft 1.8.8!

--- minecraft/GetRepositorySignature.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp GetRepositorySignature.jar net.lax1dude.signature_tool.checker.SignatureChecker
---
+++ minecraftbeta1301/src/main/java/net/minecraft/src/ModelPig.java
@@ -0,0 +1,16 @@
+package net.minecraft.src;
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) braces deadcode 
+
+public class ModelPig extends ModelQuadraped {
+
+	public ModelPig() {
+		super(6, 0.0F);
+	}
+
+	public ModelPig(float f) {
+		super(6, f);
+	}
+}
